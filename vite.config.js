2513dddf8910e9ae4b73cbb817c3f669aa945511
aruljothimoorthy <aruljothi128k@gmail.com>
2025-04-27 23:24:29 +0530

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -5,12 +5,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   base: '/E-Commerse',
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: undefined, // smaller files
-      },
-    },
-  },
   plugins: [react()],
 })
